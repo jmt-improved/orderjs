6668e372e0d12ad887b92625e8843898fd49c92a
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-12-24 20:40:25 +0100

    [new merge] draft bests calculation

--- mergeTests.js
+++ mergeTests.js
@@ -13,6 +13,8 @@ function getBests(array, offset, len, maxBests){
         arrayAllied.push(array[i]);
     }
 
+    var bests = new bestsClass(maxBests);
+
     for(var i = 0; i<totLength; i++){
         let grandPos = i;
         let arrays = arrayAllied.map((value, key)=>{
@@ -20,12 +22,52 @@ function getBests(array, offset, len, maxBests){
             let pos = grandPos%lengths[key+1];
             return value[pos];
         });
-        console.log(arrays);
+
+        bests.add(arrays, calculateScore(arrays));
+        //console.log(arrays, calculateScore(arrays));
     }
+    console.log(bests.data);
 }
 
-function mergeArrays(array1, array2){
+//fake calculator
+function calculateScore(arrays){
+    return arrays.reduce((a,b)=>a+b,0);
+}
 
+
+class bestsClass{
+
+    constructor(limit) {
+        this.limit = limit;
+        this.data = [];
+    }
+
+
+    getLastData(){
+        if(this.data.length==0)
+            return {data:[], value: 0};
+        return this.data[this.data.length-1];
+    }
+
+    add(data, value){
+        if(value>this.getLastData().value){
+            if(this.data.length>=this.limit)
+                return ;
+            this.data.push({"value": value, "data": data});
+            return ;
+        }
+
+        for(let i = 0; i<this.data.length; i++){
+            if(value<this.data[i].value){
+                let limit = Math.min(this.data.length, this.limit);
+                let newData = this.data.slice(0, i);
+                newData.push({"value": value, "data": data});
+                newData = newData.concat(this.data.slice(i,limit));
+                this.data = newData;
+                return ;
+            }
+        }
+    }
 }
 
 var arrayTest = [
